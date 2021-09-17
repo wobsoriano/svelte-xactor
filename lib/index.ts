70394eaacdddfb9502a35017cd2dc11080173342
Robert Soriano <sorianorobertc@gmail.com>
2021-09-18 02:26:02 +0800

    add types

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 import type { ActorSystem } from 'xactor';
 
-const toSvelteStore = <T extends object>(system: ActorSystem<unknown, T>) => {
+function toSvelteStore<T extends object>(system: ActorSystem<unknown, T>) {
     return {
         subscribe(fn: (payload: T) => void) {
             fn(system.getSnapshot())
